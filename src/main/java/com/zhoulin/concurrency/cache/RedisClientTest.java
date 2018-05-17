3131905325bb2e0933ad951e2fa3482585f9183f
2817748290 <2817748290@qq.com>
2018-05-17 21:52:19 +0800

    1. Redis配置和测试 2. Kafka配置和测试

---
+++ src/main/java/com/zhoulin/concurrency/cache/RedisClientTest.java
@@ -0,0 +1,16 @@
+package com.zhoulin.concurrency.cache;
+
+import org.springframework.stereotype.Component;
+import redis.clients.jedis.JedisPool;
+
+import javax.annotation.Resource;
+
+@Component
+public class RedisClientTest {
+
+    @Resource(name = "redisPool")
+    private JedisPool jedisPool;
+
+
+
+}
---
+++ src/main/java/com/zhoulin/concurrency/mq/kafka/KafkaSender.java
@@ -0,0 +1,34 @@
+package com.zhoulin.concurrency.mq.kafka;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.zhoulin.concurrency.mq.Message;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.Date;
+
+@Component
+@Slf4j
+public class KafkaSender {
+
+    @Resource
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    private Gson gson = new GsonBuilder().create();
+
+    public void send(String msg){
+
+        Message message = new Message();
+
+        message.setId(System.currentTimeMillis());
+        message.setMsg(msg);
+        message.setSendTime(new Date());
+        log.info("send message : {}" ,message);
+        kafkaTemplate.send(TopicConstants.TEST, gson.toJson(message));
+
+    }
+
+}
